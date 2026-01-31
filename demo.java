3bc36d227202d8e302a50569e3c78ffdfbe75854
Chloe <cflirio@cpp.edu>
2026-01-31 09:42:34 -0800

    third commit

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo {
-    //write a simple program.    
+       
     public static void main(String[] args) {
-    System.out.println("Hello, World!"); // comment demo
+    System.out.println("Hello, World!"); 
     }
 
 }
